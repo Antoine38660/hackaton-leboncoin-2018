60c6a6bfb58e8b626683b0580410bd1ebc1dc14a
Ysée <ysee.monnier@kaizen-solutions.net>
2018-05-25 15:27:15 +0200

    Adds project structure + iboulet for products view

---
+++ app/hackaton-leboncoin/Features/Products/ProductsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsViewController.swift
+//  hackaton-leboncoin
+//
+//  Created by Ysée Monnier on 25/05/2018.
+//  Copyright © 2018 Antoine BOISADAM. All rights reserved.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - Properties
+    
+    // MARK: - Setup
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
