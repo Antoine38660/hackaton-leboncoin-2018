2e0e096544a332286e69b9d7c0a48bbe2acaedae
Antoine Boisadam <antoineboisadam@gmail.com>
2018-05-26 09:24:21 +0200

    Buy button

--- app/hackaton-leboncoin/Features/Products/Details/ProductDetailsViewController.swift
+++ app/hackaton-leboncoin/Features/Products/Details/ProductDetailsViewController.swift
@@ -8,9 +8,40 @@
 
 import UIKit
 
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
 class ProductDetailsViewController: UIViewController {
     // MARK: - IBOutlet
-    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var adImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
